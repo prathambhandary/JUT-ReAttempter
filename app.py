56c7d705dd60dac8c63e3f4a288c4d2467b6e7ba
Pratham Bhandary <prathamb1501@gmail.com>
2026-08-04 20:57:50 +0530

    Fixed route

--- app.py
+++ app.py
@@ -197,17 +197,17 @@ def add_page():
         if data is None or not all(k in data for k in ("exam_id", "exam_type", "sequence", "exam_number")):
             return jsonify({"error": "Invalid data. Required fields: exam_id, exam_type, sequence, exam_number."}), 400
 
-        success, msg = update_github_json({
+        resp = update_github_json({
             "exam_id": data["exam_id"],
             "exam_type": data["exam_type"],
             "sequence": data["sequence"],       
             "exam_number": data["exam_number"]
         })
 
-    if success:
-        return jsonify({"success": success, "message": msg}), 200
-    
-    return jsonify({"success": success, "message": msg}), 400
+        if resp["success"]:
+            return jsonify({"success": True, "message": resp["message"]}), 200
+
+        return jsonify({"success": False, "message": resp["message"]}), 400
 
     return render_template("add_test.html")
 
