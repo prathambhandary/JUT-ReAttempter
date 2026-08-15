e8eedd2d3b467e0de5818dd7b8d03adac1b4cd72
Pratham Bhandary <pratham@firewave.in>
2026-08-15 18:54:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -772,8 +772,8 @@ def debug_receive():
 
 @app.route("/testing")
 def testroute():
-  return "This route has been deployed..."
+  return {"msg": "Still Alive...", "status": "live"}
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
