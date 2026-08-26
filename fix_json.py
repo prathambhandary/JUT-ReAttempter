5d935879dca8764e136bd53ad3d4c3db84b81733
Pratham Bhandary <prathamb1501@gmail.com>
2026-08-26 13:12:54 +0530

    Refresh JEE answerkey

--- fix_json.py
+++ fix_json.py
@@ -2,18 +2,22 @@
 import json
 
 FILE = "data/question_bank.json"
-EXAM_NUMBER = "33"
-EXAM = "KCET"
+EXAM_NUMBER = "34"
+EXAM = "JEE"
 
 with open(FILE, "r", encoding="utf-8") as f:
     data = json.load(f)
+
+ini = len(data)
 
 data = [
     item for item in data
     if item.get("exam_number") != EXAM_NUMBER or item.get("exam") != EXAM
 ]
 
+fin = len(data)
+
 with open(FILE, "w", encoding="utf-8") as f:
     json.dump(data, f, indent=4, ensure_ascii=False)
 
-print(f"Deleted elements with exam_number == {EXAM_NUMBER}")
+print(f"Deleted elements with exam_number == {EXAM_NUMBER}, {ini-fin}")
