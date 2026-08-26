5c106551e2acfeb63557a5fd1291cd0d5752e0a3
Pratham Bhandary <prathamb1501@gmail.com>
2026-08-26 16:31:31 +0530

    Retriggering

--- fix_json.py
+++ fix_json.py
@@ -4,8 +4,8 @@ FILE="data/question_bank.json"
 DATA_FILE_JEE="data/test_download_data.json"
 DATA_FILE_KCET="data/test_download_data_kcet.json"
 
-EXAM_NUMBER=""
-EXAM="KCET"
+EXAM_NUMBER="34"
+EXAM="JEE"
 EXAM_TYPE="JUT"
 
 DATA_FILE=DATA_FILE_JEE if EXAM=="JEE" else DATA_FILE_KCET
