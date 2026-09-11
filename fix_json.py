1fa3dda6c31a9e292b5521c05275d239c4d89c1c
Pratham Bhandary <pratham@firewave.in>
2026-09-11 23:19:04 +0530

    Toggle exam type between JEE and KCET

--- fix_json.py
+++ fix_json.py
@@ -6,8 +6,8 @@ DATA_FILE_JEE="data/test_download_data.json"
 DATA_FILE_KCET="data/test_download_data_kcet.json"
 
 EXAM_NUMBER="4" 
-# EXAM="JEE"
-EXAM="KCET"
+EXAM="JEE"
+# EXAM="KCET"
 EXAM_TYPE="CT"
  
 DATA_FILE=DATA_FILE_JEE if EXAM=="JEE" else DATA_FILE_KCET
